f49bf26f3c11afd813a934efac5b04d09791ab08
闫先贺 <xianhe_yan@sina.com>
2021-02-02 13:49:08 +0800

    add init linux script

---
+++ init-linux/gongan.sh
@@ -0,0 +1,35 @@
+#!/usr/bin env bash
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/software/usr/bin:/software/usr/sbin:/software/usr/local/bin:/software/usr/local/sbin
+
+filePath=`dirname $0`
+WORK_DIR=$filePath/work
+JAVA_OPTS="-XX:+UseG2GC -Xmx512m -Xms256m"
+
+PID=${WORK_DIR}/gongan-api-server.pid
+
+funcStart() {
+    nohup java -jar ${JAVA_OPTS} ${WORK_DIR}/gongan-0.0.1-SNAPSHOT.jar 2>&1 >/dev/null &
+    if [ ! -z "${PID}" ]; then
+      echo $! > ${PID}
+    fi
+}
+funcStop() {
+    kill `cat ${PID}` && rm ${PID}
+}
+
+if [ "$1" = "start" ] ; then
+    funcStart
+elif [ "$1" = "stop" ] ; then
+    funcStop
+elif [ "$1" = "restart" ] ; then
+    funcStop
+    funcStart
+else
+  echo "Usage: gongan.sh ( commands ... )"
+  echo "commands:"
+  echo "  start             Start Auth API Server"
+  echo "  stop              Stop Auth API Server"
+  echo "  restart           Restart Auth API Server"
+  exit 1
+
+fi
